8efa6af98ba1afdfb81f4735cb209a2f19ebc30f
ColtKing <king.colt@yahoo.ca>
2017-04-06 22:58:36 -0700

    Made the Menu model depend on the backend

--- application/views/template.php
+++ application/views/template.php
@@ -8,13 +8,13 @@
 		{caboose_styles}
 	</head>
     <body>
-		<div class="container">
-			{navbar}
+        <div class="container">
+            {navbar}
             {content}
             <p class="footer">Page rendered in <strong>0.0155</strong> seconds. 
                 {ci_version}</p>
         </div>
         {caboose_scripts}
-		{caboose_trailings}
+        {caboose_trailings}
     </body>
 </html>
